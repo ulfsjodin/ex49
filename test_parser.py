93712235ae1a058b380db485d3d0516acddc197b
Ulf Sjodin <sjodin.ulf@gmail.com>
2017-11-01 22:54:57 +0100

    Update test_parser.py

--- test_parser.py
+++ test_parser.py
@@ -2,7 +2,7 @@ import pytest
 from parser import *
 
 def test_parse_sentence():
-    x = parse_sentence([('noun','princess'),('verb','run'),('direction','away')$
+    x = parse_sentence([('noun','princess'),('verb','run'),('direction','away')
     assert x.subject == 'princess'
     assert x.verb == 'run'
     assert x.obj == 'away'

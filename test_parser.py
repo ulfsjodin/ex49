cd88aaeaf9c7117ca4f030eb49762660330f5ac3
Ulf Sjodin <sjodin.ulf@gmail.com>
2017-11-01 22:54:26 +0100

    Update test_parser.py

--- test_parser.py
+++ test_parser.py
@@ -1,39 +1,11 @@
 import pytest
-import parser
-from parser import Sentence
-
-def test_sentence():
-    a = Sentence('a', 'b', '3', 'd')
-
-    assert a.subject == 'a'
-    assert a.verb == 'b'
-    assert a.number == '3'
-    assert a.obj == 'd'
-
-def test_peek():
-    pass
-
-def test_match():
-    pass
-
-def test_skip():
-    pass
-
-def test_Parse():
-    pass
-
-def test_parse_verb():
-    pass
-
-def test_parse_number():
-    pass
-
-def test_parse_object():
-    pass
-
-def test_parse_subject():
-    pass
+from parser import *
 
 def test_parse_sentence():
-    pass
+    x = parse_sentence([('noun','princess'),('verb','run'),('direction','away')$
+    assert x.subject == 'princess'
+    assert x.verb == 'run'
+    assert x.obj == 'away'
 
+def test_parser_error():
+    pass                    
